077a0dc03769bbc42bf944d5feb37e719ebbed8d
MANU <singhaldeoli103@gmail.com>
2025-07-15 12:26:41 +0000

    3D Scene Performance:

--- src/components/3d/castle-scene.tsx
+++ src/components/3d/castle-scene.tsx
@@ -7,60 +7,46 @@ function Tower({ position }: { position: [number, number, number] }) {
   return (
     <group position={position}>
       <mesh>
-        <cylinderGeometry args={[1, 1, 6, 8]} />
+        <cylinderGeometry args={[0.8, 1, 5, 6]} />
         <meshStandardMaterial color="#5c5c5c" />
       </mesh>
-      <mesh position={[0, 3.5, 0]}>
-        <coneGeometry args={[1.2, 1, 8]} />
+      <mesh position={[0, 3, 0]}>
+        <coneGeometry args={[1.2, 1.5, 6]} />
         <meshStandardMaterial color="#8b4513" />
       </mesh>
     </group>
   );
 }
 
-function Wall({ position, args }: { position: [number, number, number]; args: [number, number, number] }) {
-    return (
-        <mesh position={position}>
-            <boxGeometry args={args} />
-            <meshStandardMaterial color="#6b6b6b" />
-        </mesh>
-    )
-}
-
 function Castle() {
   return (
-    <group position={[0, -2, -5]}>
-      {/* Main Keep */}
-      <mesh position={[0, 2, 0]}>
-        <boxGeometry args={[4, 4, 4]} />
+    <group position={[0, -2.5, -5]}>
+      {/* Simplified Main Keep */}
+      <mesh position={[0, 1.5, 0]}>
+        <boxGeometry args={[5, 3, 4]} />
         <meshStandardMaterial color="#7c7c7c" />
       </mesh>
-       <mesh position={[0, 4, 0]}>
-        <cylinderGeometry args={[0, 2.5, 2, 4]} />
+       <mesh position={[0, 3.5, 0]}>
+        <cylinderGeometry args={[0, 3, 1.5, 4]} />
         <meshStandardMaterial color="#8b4513" />
       </mesh>
 
       {/* Towers */}
-      <Tower position={[-3, 3, -3]} />
-      <Tower position={[3, 3, -3]} />
-      <Tower position={[-3, 3, 3]} />
-      <Tower position={[3, 3, 3]} />
-
-      {/* Walls */}
-      <Wall position={[0, 1.5, 4]} args={[8, 3, 0.5]} />
-      <Wall position={[0, 1.5, -4]} args={[8, 3, 0.5]} />
-      <Wall position={[5, 1.5, 0]} args={[0.5, 3, 8]} />
-      <Wall position={[-5, 1.5, 0]} args={[0.5, 3, 8]} />
+      <Tower position={[-2.5, 2.5, -1.5]} />
+      <Tower position={[2.5, 2.5, -1.5]} />
+      <Tower position={[-2.5, 2.5, 1.5]} />
+      <Tower position={[2.5, 2.5, 1.5]} />
       
-      <DreiSparkles count={50} scale={10} size={2} speed={0.4} color="#FFC857" />
+      {/* Reduced particle count */}
+      <DreiSparkles count={30} scale={12} size={3} speed={0.3} color="#FFC857" />
     </group>
   );
 }
 
 export function CastleScene() {
   return (
-    <Canvas camera={{ position: [0, 5, 20], fov: 60 }}>
-      <ambientLight intensity={0.3} />
+    <Canvas camera={{ position: [0, 4, 18], fov: 60 }}>
+      <ambientLight intensity={0.4} />
       <directionalLight 
         position={[5, 10, 7.5]} 
         intensity={1.2} 
@@ -72,9 +58,9 @@ export function CastleScene() {
         enableZoom={false} 
         enablePan={false}
         autoRotate 
-        autoRotateSpeed={0.2}
-        minPolarAngle={Math.PI / 4}
-        maxPolarAngle={Math.PI / 2}
+        autoRotateSpeed={0.3}
+        minPolarAngle={Math.PI / 3}
+        maxPolarAngle={Math.PI / 2.2}
       />
     </Canvas>
   );
